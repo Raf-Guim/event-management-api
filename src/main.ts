e7d927c6d9be43ec432164006641c60311e4e224
Rafael Prado Lopes Guimaraes <raf.plguimaraes@gmail.com>
2024-05-14 20:55:49 -0300

    feat: Added database module and swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Management Api')
+    .setDescription(
+      'This service is responsible for management of events and its participants.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
+
+  console.log(`🚀 Application is running on: http://localhost:${port}/docs`);
 }
 bootstrap();
